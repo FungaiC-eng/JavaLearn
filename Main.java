99d7095b28b4fb793a23bd8be2066734e3211b71
FungaiC-eng <f.changunda@gmail.com>
2024-10-06 08:41:00 +0200

    adding Java class and main files

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+  public static void main(String[] args) {
+    //first code
+    System.out.println("Hello World, allow me to introduce myself... The name is FUNGAI CHANGUNDA");
+    
+    //more follow up code. Numbers
+    System.out.println(13 *350);
+
+    int x = 5, y= 7, z = 11;
+    System.out.println(x + y + z);
+
+    //boolean code
+    boolean isLifeFair = true;
+    System.out.println(isLifeFair);  
+
+    // Create variables of different data types
+    int items = 100;
+    float costPerItem = 9.99f;
+    float totalCost = items * costPerItem;
+    char currency = 'R';
+
+    // Print variables
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + currency + costPerItem);
+    System.out.println("Total cost = " + currency + totalCost);  
+
+  }
+}
